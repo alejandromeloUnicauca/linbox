532ac9b8b155c019857139442299871c593efcad
wezzman <mwezz@udel.edu>
2017-04-03 16:05:07 -0400

    Implemented a matrix multiplication test. Indexing math has now been tested and passes.

--- tests/test-blocked.C
+++ tests/test-blocked.C
@@ -35,52 +35,88 @@
 
 #include "linbox/linbox-config.h"
 
-#include "linbox/util/commentator.h"
-#include "linbox/ring/modular.h"
 #include "linbox/matrix/blockedmatrix/dense-block-allocator.h"
 #include "linbox/matrix/blockedmatrix/blocked-matrix.h"
+#include "linbox/util/commentator.h"
+#include "linbox/ring/modular.h"
 
 #include <iostream>
+#include <string>
+#include <sys/time.h>
 
 using namespace LinBox;
 
+template<class _F, class _M>
+void blockedMul(const _F& F, _M& C, _M& A, _M& B){
+	typedef typename _M::Block_t Block_t;
+	BlasMatrixDomain<_F> BMD(F);
+
+	for(size_t i = 0; i < C.blocksPerColumn(); i++){
+		for(size_t j = 0; j < C.blocksPerRow(); j++){
+			Block_t& C_ij = C.refBlock(i,j);
+			for(size_t k = 0; k < A.blocksPerRow(); k++){
+				const Block_t& A_ik = A.getBlock(i,k);
+				const Block_t& B_kj = B.getBlock(k,j);
+
+				BMD.axpyin(*C_ij,*A_ik,*B_kj);
+			}
+		}
+	}
+}
+
 int main (int argc, char **argv){
+	typedef Givaro::Modular<double> Field;
+	
 	bool pass = true;
 	static size_t n = 128;
 	static int q = 100003U;
-
-	typedef Givaro::Modular<double> Field;
 	Field F(q);
+
+	static Argument args[] = {
+		{'n', "-n N", "Set dimension of the test matrices to NxN", TYPE_INT, &n},
+		END_OF_ARGUMENTS
+	};
+
+	parseArguments(argc, argv, args);
+
+	srand((unsigned)time(NULL));
 
 	commentator().getMessageClass(INTERNAL_DESCRIPTION).setMaxDepth (5);
 	commentator().getMessageClass(INTERNAL_DESCRIPTION).setMaxDetailLevel(Commentator::LEVEL_UNIMPORTANT);
 
 	commentator().start("Blocked matrix test suite", "BlockedMatrix");
-	
-	BlockedMatrix<Field, DenseBlockAllocator<Field> > A1(F,n,n,n/2,n/2);
-	BlockedMatrix<Field, DenseBlockAllocator<Field> > B1(F,n,n,n/2,n/2);
-	BlockedMatrix<Field, DenseBlockAllocator<Field> > C1(F,n,n,n/2,n/2);
+
+	BlasMatrixDomain<Field> BMD(F);
+
+	BlockedMatrix<Field, DenseBlockAllocator<Field> > A1(F,2,2,n/2,n/2);
+	BlockedMatrix<Field, DenseBlockAllocator<Field> > B1(F,2,2,n/2,n/2);
+	BlockedMatrix<Field, DenseBlockAllocator<Field> > C1(F,2,2,n/2,n/2);
 
 	BlasMatrix<Field> A2(F,n,n);
 	BlasMatrix<Field> B2(F,n,n);
 	BlasMatrix<Field> C2(F,n,n);
+	BlasMatrix<Field> C_temp(F,n,n);
 
-	Field::Element tmp;
 	Field::RandIter G(F);
 
 	for(size_t i = 0; i < n; i++){
 		for(size_t j = 0; j < n; j++){
-			tmp = G.random();
-			A1.setEntry(i,j,tmp);
-			A2.setEntry(i,j,tmp);
-
-			tmp = G.random();
-			B1.setEntry(i,j,tmp);
-			B2.setEntry(i,j,tmp);
+			A2.setEntry(i,j,G.random());
+			B2.setEntry(i,j,G.random());
 		}
 	}
 
-	commentator().stop("Blocked matrix test suite");
+	A1.copyFromMatrix<BlasMatrix<Field> >(A2);
+	B1.copyFromMatrix<BlasMatrix<Field> >(B2);
+
+	BMD.mul(C2,A2,B2);
+	blockedMul(F,C1,A1,B1);
+
+	C1.copyToMatrix(C_temp);
+
+	pass = BMD.areEqual(C2,C_temp);
+
+	commentator().stop(MSG_STATUS(pass), " Blocked matrix test suite", (const char*)0);
 	return pass ? 0 : -1;
 }
 
